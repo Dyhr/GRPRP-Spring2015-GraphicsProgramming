439f82523d7621d924c8c49cef6d7ad124af20c9
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-04-10 12:51:18 +0200

    Copy pasta byte array to bitmap conversion

--- code/RayTracer/WindowApplication/MainWindow.xaml.cs
+++ code/RayTracer/WindowApplication/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,41 @@ namespace WindowApplication
 
             var scene = new Scene();
             scene.init(800,600);
-            scene.render();
+            var data = scene.render();
+            var bytearr = ConvertData(data);
+            var img = LoadImage(bytearr);
+            Canvas.Source = img;
+        }
+
+        private byte[] ConvertData(Color[] data)
+        {
+            var output = new List<byte>();
+            foreach (var color in data)
+            {
+                output.Add((byte)color.red);
+                output.Add((byte)color.blue);
+                output.Add((byte)color.green);
+                output.Add((byte)color.alpha);
+            }
+            return output.ToArray();
+        }
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
         }
     }
 }
