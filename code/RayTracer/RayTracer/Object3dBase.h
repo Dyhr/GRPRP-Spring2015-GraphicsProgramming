ed55b7cddc23865b4faf5d7a9ba77c2d90b5a9b6
Anders Wind Steffensen <awis@itu.dk>
2015-04-13 00:13:45 +0200

    Just added some details to the last class im adding today.

--- code/RayTracer/RayTracer/Object3dBase.h
+++ code/RayTracer/RayTracer/Object3dBase.h
@@ -1,12 +1,13 @@
 #pragma once
+#include "Line3d.h"
 #include "Vector3d.h"
 namespace RayTracer{
 	ref class Object3dBase
 	{
 	public:
 		Object3dBase();
-		virtual Vector3d CalculateNormal();
-		virtual Vector3d CalculateCollisionPosition();
+		virtual Vector3d CalculateNormal(Vector3d); // normal based on position
+		virtual Vector3d CalculateCollisionPosition(Line3d);
 	};
 }
 
--- code/RayTracer/RayTracer/Sphere3d.h
+++ code/RayTracer/RayTracer/Sphere3d.h
@@ -5,8 +5,11 @@ namespace RayTracer{
 	ref class Sphere3d :
 		public Object3dBase
 	{
+		const Vector3d centerPosition;
+		const float radius;
 	public:
 		Sphere3d();
+		Sphere3d(Vector3d, float);
 	};
 }
 
