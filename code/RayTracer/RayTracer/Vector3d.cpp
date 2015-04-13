8c4a7d0295c1ffb9949e87b79c3fed48481dedc5
Morten Albertsen <moalb@itu.dk>
2015-04-13 22:58:53 +0200

    Work on implementing methods in Vector3d.cpp

--- code/RayTracer/RayTracer/Vector3d.cpp
+++ code/RayTracer/RayTracer/Vector3d.cpp
@@ -1,8 +1,141 @@
 #include "stdafx.h"
 #include "Vector3d.h"
+#include <cmath>
 
-namespace RayTracer{
-	Vector3d::Vector3d()
+#define PI float(3.14) 
+
+namespace RayTracer
+{
+
+	// The empty constructor: Set default values for components
+	Vector3d::Vector3d() : x(float(0.0)), y(float(0.0)), z(float(0.0)), length(sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)))
 	{
+
+	}
+
+	Vector3d::Vector3d(float x1, float y1, float z1) : x(x1), y(y1), z(z1), length(sqrt(pow(x1, 2) + pow(y1, 2) + pow(z1, 2)))
+	{
+
+	}
+
+	Vector3d::Vector3d(Vector3d endPoint, Vector3d startPoint) : 
+		x(float(endPoint.x - startPoint.x)), y(float(endPoint.y - startPoint.y)), 
+		z(float(endPoint.z-startPoint.z)),
+		length(float(sqrt(pow((endPoint.x - startPoint.x), 2) + pow((endPoint.y - startPoint.y), 2) + pow((endPoint.z - startPoint.z), 2))))
+	{
+
+	}
+
+	Vector3d Vector3d::normalizeVector()
+	{
+		if (length == 0.0f)
+		{
+			// We cannot divide by zero
+			// Return a zero-length vector
+			Vector3d returnValue = Vector3d();
+			return returnValue;
+		}
+
+		float normalisedX = x / length;
+		float normalisedY = y / length;
+		float normalisedZ = z / length;
+
+		Vector3d normalisedVector = Vector3d(normalisedX, normalisedY, normalisedZ);
+		return normalisedVector;
+	}
+
+	Vector3d Vector3d::negateVector()
+	{
+		float xNegated = x * (-1.0);
+		float yNegated = y * (-1.0);
+		float zNegated = z * (-1.0);
+
+		Vector3d negatedVector = Vector3d(xNegated, yNegated, zNegated);
+		return negatedVector;
+	}
+
+	Vector3d Vector3d::vectorTimesScalar(float scalar)
+	{
+		float scaledX = x * scalar;
+		float scaledY = y * scalar;
+		float scaledZ = z * scalar;
+
+		Vector3d scaledVector = Vector3d(scaledX, scaledY, scaledZ);
+		return scaledVector;
+	}
+
+
+	Vector3d Vector3d::addition(Vector3d v1, Vector3d v2)
+	{
+		float newX = v1.x + v2.x;
+		float newY = v1.y + v2.y;
+		float newZ = v1.z + v2.z;
+
+		return Vector3d(newX, newY, newZ);
+	}
+
+	// Since, header-file declares this method as static, there's no need to repeat that info; http://stackoverflow.com/questions/15725922/static-function-a-storage-class-may-not-be-specified-here
+	Vector3d Vector3d::subtraction(Vector3d v1, Vector3d v2)
+	{
+		float newX = v1.x - v2.x;
+		float newY = v1.y - v2.y;
+		float newZ = v1.z - v2.z;
+
+		return Vector3d(newX, newY, newZ);
+	}
+
+	Vector3d Vector3d::reflectionVector(Vector3d normal, Vector3d incoming)
+	{
+		// Define exiting vector originating from the origin of normal
+		// http://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+		Vector3d step1 = incoming.vectorTimesScalar(2.0);			// 2*a
+		float step2 = dotProduct(step1, normal);					// 2*a*n
+		float step3 = step2 / pow(normal.length, 2.0);				// (2*a*n)/(|n|^2)
+		Vector3d step4 = vectorTimesScalar(normal, step3);			// ((2*a*n)/(|n|^2)) x n
+		Vector3d final = subtraction(incoming, step4);				// a - ( ((2*a*n)/(|n|^2)) x n )
+
+		return final;
+	}
+
+	Vector3d Vector3d::refractionVector(Vector3d, Vector3d, float, float) // normal and an incoming - refractionindex of from and to material
+	{
+		// Dummy implementation: Haven't had time to look into this yet!
+		return Vector3d();
+	}
+
+	float dotProduct(Vector3d v1, Vector3d v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	}
+
+	float cosV(Vector3d v1, Vector3d v2)
+	{
+		// The two are normalized
+		Vector3d normV1 = v1.normalizeVector();
+		Vector3d normV2 = v2.normalizeVector();
+
+		float dotP = dotProduct(normV1, normV2);
+		return cos(dotP);
+	}
+
+	float distanceBetweenPoints(Vector3d v1, Vector3d v2)
+	{
+		float x1 = v1.x;
+		float y1 = v1.y;
+		float z1 = v1.z;
+		
+		float x2 = v2.x;
+		float y2 = v2.y;
+		float z2 = v2.z;
+
+		return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) + pow((z2 - z1), 2));
+	}
+
+	// Notice, that isSameDirection returns false if v1 and v2 are parallel but pointing in opposite directions
+	bool isSameDirection(Vector3d v1, Vector3d v2)
+	{
+		// TODO: Define global threshold value
+		float threshold = 0.001;
+		return abs(dotProduct(v1,v2) - 1.0 ) < threshold;
 	}
 }
--- code/RayTracer/RayTracer/Vector3d.h
+++ code/RayTracer/RayTracer/Vector3d.h
@@ -1,26 +1,29 @@
 #pragma once
 namespace RayTracer{
 	// The Vector3d class sh
-	ref class Vector3d
+	class Vector3d
 	{
-		const float x, y, z;
-		const float length;
-	
-	public:
+		public:
+		// Denotes either a) a point in space (in which case length must be 0) or b) a vector with a direction
+		const float x, y, z, length;
 		Vector3d(); // the empty constructor.
-		Vector3d(float, float, float);
-		Vector3d(Vector3d, Vector3d);
+		Vector3d(float, float, float);	// This represents a) a point or b) a vector
+		Vector3d(Vector3d, Vector3d);	// This represents a parametrization: (x,y,z) = (x0,y0,z0) + t*(x1,y1,z1) <- Nope, Wrong! 
 		Vector3d normalizeVector();
 		Vector3d negateVector();
 		Vector3d vectorTimesScalar(float);
 
 		static Vector3d addition(Vector3d, Vector3d);
-		static Vector3d subtraction(Vector3d, Vector3d);
-		static Vector3d multiplication(Vector3d, Vector3d);
-		static Vector3d reflectionVector(Vector3d, Vector3d); // normal and an incoming
+
+		// Important: subtraction() performs the subtraction v1 - v2; NOT v2 - v1
+		static Vector3d subtraction(Vector3d v1, Vector3d v2);
+		static Vector3d reflectionVector(Vector3d normal, Vector3d incoming); // normal and an incoming
+		static Vector3d vectorTimesScalar(Vector3d vector, float scalar);
 		static Vector3d refractionVector(Vector3d, Vector3d, float, float); // normal and an incoming - refractionindex of from and to material
-		static float dotProduct(Vector3d, Vector3d);
-		static float cosV(Vector3d, Vector3d);
+		static Vector3d normalizeVector(Vector3d vectorToBeNormalized);
+		static float dotProduct(Vector3d v1, Vector3d v2);
+		// Determines cos(v), where v denotes the angle between v1 and v2
+		static float cosV(Vector3d v1, Vector3d v2);
 		static float distanceBetweenPoints(Vector3d, Vector3d); 
 		static bool isSameDirection(Vector3d, Vector3d);
 	};
