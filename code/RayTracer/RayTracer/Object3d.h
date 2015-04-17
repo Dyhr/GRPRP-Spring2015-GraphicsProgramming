06e80ed8c46518e5f3fe27f96a01615a02c3c18a
Anders Wind Steffensen <awis@itu.dk>
2015-04-17 09:40:53 +0200

    Cleared the point interface and fix a compiler error

--- code/RayTracer/RayTracer/Object3d.h
+++ code/RayTracer/RayTracer/Object3d.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 namespace RayTracer{
-	ref class Object3d
+	class Object3d
 	{
 	public:
 		virtual Vector3d CalculateNormal(Vector3d) = 0; // normal based on position.. what?
--- code/RayTracer/RayTracer/Plane3d.h
+++ code/RayTracer/RayTracer/Plane3d.h
@@ -1,10 +1,23 @@
 #pragma once
+#include "Object3d.h"
+#include "Point3d.h"
+#include "Vector3d.h"
+#include "ShaderBase.h"
+
 namespace RayTracer
 {
-	ref class Plane3d
+	class Plane3d
 	{
+		const Vector3d normal;
+		const Point3d position;
+		ShaderBase* shader;
 	public:
 		Plane3d();
+		Plane3d(Point3d point, Vector3d normal, ShaderBase* shader);
+
+		Vector3d CalculateNormal(Vector3d); // normal based on position always the same for a plane.
+		Vector3d CalculateCollisionPosition(Line3d);
+		ColorIntern shadeThis(Vector3d eyeVector, Vector3d normalToSurface, Vector3d pointOnObject, vector<LightBase*> lights);
 	};
 
 }
