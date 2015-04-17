8320ede1820d572360c6a5e1ea6941d904a9988e
Morten Albertsen <moalb@itu.dk>
2015-04-17 09:32:15 +0200

    Added empty SpecularShader; will implement in the following

---
+++ code/RayTracer/RayTracer/SpecularShader.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "SpecularShader.h"
+
+namespace RayTracer
+{
+	SpecularShader::SpecularShader()
+	{
+	}
+}
+
---
+++ code/RayTracer/RayTracer/SpecularShader.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ShaderBase.h"
+namespace RayTracer
+{
+	class SpecularShader : public ShaderBase
+	{
+	public:
+		SpecularShader();
+	};
+}
+
+
