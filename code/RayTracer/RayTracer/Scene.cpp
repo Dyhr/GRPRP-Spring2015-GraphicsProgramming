04335bf016d88bb8c0cc8a385cab2eb269e7cef8
Anders Wind Steffensen <awis@itu.dk>
2015-04-20 14:08:44 +0200

    Changed the scene such that it looks like the cornell box

--- code/RayTracer/RayTracer/Scene.cpp
+++ code/RayTracer/RayTracer/Scene.cpp
@@ -32,7 +32,7 @@ namespace RayTracer {
 
 	ColorIntern Scene::ambientColorOnObjects()
 	{
-		ColorIntern ambientColor = ColorIntern(40, 40, 40, 255);
+		ColorIntern ambientColor = ColorIntern(200, 200, 200, 255);
 		return ambientColor;
 	}
 
@@ -54,34 +54,46 @@ namespace RayTracer {
 
 	vector<Object3d*> sceneObjects;
 	vector<LightBase*> lightObjects;
-	vector<ShaderBase*> shadersOnObject1;
-	vector<ShaderBase*> shadersOnObject2;
+	vector<ShaderBase*> shadersWhite;
+	vector<ShaderBase*> shadersRed;
+	vector<ShaderBase*> shadersGreen;
 
 	array<Color^>^ Scene::render()
 	{
-		sceneObjects = vector<Object3d*>(4);
-		shadersOnObject1 = vector<ShaderBase*>(2);
-		shadersOnObject2 = vector<ShaderBase*>(2);
+		sceneObjects = vector<Object3d*>(7);
 
-		shadersOnObject1[0] = new AmbientShader(ambientColorOnObjects());
-		shadersOnObject1[1] = new DiffuseShader(ColorIntern(255, 0, 255, 255));
+		shadersWhite = vector<ShaderBase*>(2);
+		shadersRed = vector<ShaderBase*>(2);
+		shadersGreen = vector<ShaderBase*>(2);
 
-		shadersOnObject2[0] = new AmbientShader(ambientColorOnObjects());
-		shadersOnObject2[1] = new DiffuseShader(ColorIntern(0, 255, 255, 255));
-		//shadersOnObject1[2] = new SpecularShader(ColorIntern(240, 240, 240, 255), 10);
+		shadersWhite[0] = new AmbientShader(ColorIntern(255, 240, 245, 255));
+		shadersWhite[1] = new DiffuseShader(ColorIntern(255, 240, 245, 255));
 
-		sceneObjects[0] = new Sphere3d(Point3d(0, -0.3f, 10), 1, shadersOnObject1);
-		sceneObjects[1] = new Sphere3d(Point3d(-2, 0, 8), 1, shadersOnObject1);
-		sceneObjects[2] = new Sphere3d(Point3d(0, 2.1f, 10), 1, shadersOnObject2);
-		//sceneObjects[2] = new Sphere3d(Point3d(80, 120, 10), 20, shadersOnObject1);
-		sceneObjects[3] = new Plane3d(Point3d(0,-1,0), Vector3d(0,1,0), shadersOnObject1);
+		shadersRed[0] = new AmbientShader(ColorIntern(235, 45, 20, 255));
+		shadersRed[1] = new DiffuseShader(ColorIntern(235, 45, 20, 255));
+
+		shadersGreen[0] = new AmbientShader(ColorIntern(30, 235, 55, 255));
+		shadersGreen[1] = new DiffuseShader(ColorIntern(30, 235, 55, 255));
+		
+		// planes
+		sceneObjects[0] = new Plane3d(Point3d(0, -3, 0), Vector3d(0, 1, 0), shadersWhite);
+		sceneObjects[1] = new Plane3d(Point3d(0, 0, 15), Vector3d(0, 0, -1), shadersWhite);
+		sceneObjects[2] = new Plane3d(Point3d(0, 5, 0), Vector3d(0, -1, 0), shadersWhite);
+		sceneObjects[3] = new Plane3d(Point3d(-5, 0, 0), Vector3d(1, 0, 0), shadersRed);
+		sceneObjects[4] = new Plane3d(Point3d(5, 0, 0), Vector3d(-1, 0, 0), shadersGreen);
+
+		sceneObjects[5] = new Sphere3d(Point3d(-2, -2, 8), 1, shadersWhite);
+		sceneObjects[6] = new Sphere3d(Point3d(2, -1, 12), 2, shadersWhite);
+
+
 		//sceneObjects[4] = new Triangle3d(Point3d(-6, 3.8f, 10), Point3d(0, 4.5f, 12), Point3d(-2, 2.2f, 5), shadersOnObject2);
 
 		lightObjects = vector<LightBase*>(3);
-		lightObjects[0] = new AmbientLight(0.2f);
-		lightObjects[1] = new DirectionalLight(0.3f, Vector3d(0.5f, -1, 0.3f), ColorIntern(255,150,180,255));
+		lightObjects[0] = new AmbientLight(0.15f);
+		//lightObjects[1] = new DirectionalLight(0.3f, Vector3d(0.5f, -1, 0.3f), ColorIntern(255,150,180,255));
 		//lightObjects[2] = new DirectionalLight(0.5f, Vector3d(0.0f, -0.1f, 1.0f), ColorIntern(180, 150, 255, 255));
-		lightObjects[2] = new PositionalLight(0.9f, Point3d(5, 0, 3), 14.0f, ColorIntern(100,255,255,255));
+		lightObjects[1] = new PositionalLight(0.75f, Point3d(0, 4, 10), 10.0f, ColorIntern(255,230,230,255));
+		lightObjects[2] = new PositionalLight(0.2f, Point3d(0, 0, 0), 10.0f, ColorIntern(255, 255, 255, 255));
 
 		// This is where the magic happens: main-loop!
 		for (int x = 0; x < width; x++)
