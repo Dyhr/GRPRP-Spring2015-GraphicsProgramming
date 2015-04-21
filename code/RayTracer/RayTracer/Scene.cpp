22e746a21f532474ebaa36aea625c22a5753e63f
Anders Wind Steffensen <awis@itu.dk>
2015-04-21 11:08:46 +0200

    faked global illumination

--- code/RayTracer/RayTracer/Scene.cpp
+++ code/RayTracer/RayTracer/Scene.cpp
@@ -55,6 +55,7 @@ namespace RayTracer {
 	vector<Object3d*> sceneObjects;
 	vector<LightBase*> lightObjects;
 	vector<ShaderBase*> shadersWhite;
+	vector<ShaderBase*> shadersWhiteSpecular;
 	vector<ShaderBase*> shadersRed;
 	vector<ShaderBase*> shadersGreen;
 
@@ -63,12 +64,16 @@ namespace RayTracer {
 		sceneObjects = vector<Object3d*>(7);
 
 		shadersWhite = vector<ShaderBase*>(2);
+		shadersWhiteSpecular = vector<ShaderBase*>(3);
 		shadersRed = vector<ShaderBase*>(2);
-		shadersGreen = vector<ShaderBase*>(3);
+		shadersGreen = vector<ShaderBase*>(2);
 
 		shadersWhite[0] = new AmbientShader(ColorIntern(255, 240, 245, 255));
 		shadersWhite[1] = new DiffuseShader(ColorIntern(255, 240, 245, 255));
-		shadersGreen[2] = new SpecularShader(ColorIntern(255, 255, 255, 255),5.0f);
+
+		shadersWhiteSpecular[0] = new AmbientShader(ColorIntern(255, 240, 245, 255));
+		shadersWhiteSpecular[1] = new DiffuseShader(ColorIntern(255, 240, 245, 255));
+		shadersWhiteSpecular[2] = new SpecularShader(ColorIntern(255, 255, 255, 255),5.0f);
 
 		shadersRed[0] = new AmbientShader(ColorIntern(235, 45, 20, 255));
 		shadersRed[1] = new DiffuseShader(ColorIntern(235, 45, 20, 255));
@@ -83,18 +88,21 @@ namespace RayTracer {
 		sceneObjects[3] = new Plane3d(Point3d(-5, 0, 0), Vector3d(1, 0, 0), shadersRed);
 		sceneObjects[4] = new Plane3d(Point3d(5, 0, 0), Vector3d(-1, 0, 0), shadersGreen);
 
-		sceneObjects[5] = new Sphere3d(Point3d(-1, -2, 8), 1, shadersWhite,Material(0.1f));
-		sceneObjects[6] = new Sphere3d(Point3d(2, -1, 12), 2, shadersGreen,Material(0.4f));
+		sceneObjects[5] = new Sphere3d(Point3d(-1, -2, 8), 1, shadersWhiteSpecular, Material());
+		sceneObjects[6] = new Sphere3d(Point3d(2, -1, 12), 2, shadersWhiteSpecular, Material(0.4f));
 
 
 		//sceneObjects[4] = new Triangle3d(Point3d(-6, 3.8f, 10), Point3d(0, 4.5f, 12), Point3d(-2, 2.2f, 5), shadersOnObject2);
 
-		lightObjects = vector<LightBase*>(3);
+		lightObjects = vector<LightBase*>(5);
 		lightObjects[0] = new AmbientLight(0.15f);
 		//lightObjects[1] = new DirectionalLight(0.3f, Vector3d(0.5f, -1, 0.3f), ColorIntern(255,150,180,255));
 		//lightObjects[2] = new DirectionalLight(0.5f, Vector3d(0.0f, -0.1f, 1.0f), ColorIntern(180, 150, 255, 255));
 		lightObjects[1] = new PositionalLight(0.75f, Point3d(0, 4, 10), 10.0f, ColorIntern(255,230,230,255));
 		lightObjects[2] = new PositionalLight(0.2f, Point3d(0, 0, 0), 10.0f, ColorIntern(255, 255, 255, 255));
+
+		lightObjects[3] = new PositionalLight(0.2f, Point3d(-4.0f, 0, 10), 2.0f, ColorIntern(235, 45, 20, 255));
+		lightObjects[4] = new PositionalLight(0.2f, Point3d(4.5f, 0, 10), 2.0f, ColorIntern(30, 235, 55, 255));
 
 		// This is where the magic happens: main-loop!
 		for (int x = 0; x < width; x++)
